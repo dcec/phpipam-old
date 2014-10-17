68a2dff5351cbe5b03f288ded9db49c3f98a51f1
Davide Cecchetto <dcecchetto@gmail.com>
2014-10-17 11:42:47 +0200

    db config

--- config.php
+++ config.php
@@ -3,9 +3,49 @@
 /*	database connection details
  ******************************/
 $db['host'] = "localhost";
-$db['user'] = "phpipam";
-$db['pass'] = "phpipamadmin";
+$db['user'] = "root";
+$db['pass'] = "";
 $db['name'] = "phpipam";
+
+/* glpi database connection details
+LEAVE EMPTY IF NOT USING GLPI
+************************************/
+$db['glpi_host'] = "10.55.68.4";
+$db['glpi_user'] = "phpipam";
+$db['glpi_pass'] = "";
+$db['glpi_name'] = "glpi";
+
+/* glpi url
+e.g. www.myglpi.com
+192.168.1.1
+************************************/
+$glpiurl = '';
+
+/* glpi discovery subnets
+$glpisubnets = '192.168.132.0/24,127.0.0.1/30'
+separate the subnets with a coma
+**************************************************/
+$glpisubnets = '';
+
+/* glpi database connection details
+LEAVE EMPTY IF NOT USING GLPI
+************************************/
+$db['nedi_host'] = "10.55.68.4";
+$db['nedi_user'] = "phpipam";
+$db['nedi_pass'] = "";
+$db['nedi_name'] = "nedi";
+
+/* glpi url
+e.g. www.myglpi.com
+192.168.1.1
+************************************/
+#$nediurl = '';
+
+/* glpi discovery subnets
+$glpisubnets = '192.168.132.0/24,127.0.0.1/30'
+separate the subnets with a coma
+**************************************************/
+#$nedisubnets = '';
 
 /**
  * php debugging on/off
@@ -13,7 +53,7 @@ $db['name'] = "phpipam";
  * true  = SHOW all php errors
  * false = HIDE all php errors
  ******************************/
-$debugging = false;
+$debugging = true;
 
 /**	
  *	BASE definition if phpipam 
@@ -22,6 +62,6 @@ $debugging = false;
  *  Also change 
  *	RewriteBase / in .htaccess
  ******************************/
-define('BASE', "/");
+define('BASE', "/phpipam-code/");
 
 ?>
